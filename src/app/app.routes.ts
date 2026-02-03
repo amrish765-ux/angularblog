4f117ff052278e04be83f5ca0619f0d402bb055f
amrish765-ux <amrish01011999@gmail.com>
2026-02-03 06:34:39 +0530

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { ProfileComponent } from './components/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{path:'',component:ProfileComponent}];
